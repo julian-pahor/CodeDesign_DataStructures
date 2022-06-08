adafcd2fb3e7de8e0053a87c19a7897a5cd09378
s214615 <s214615@ADL-43220D>
2022-06-08 15:49:15 +0930

    Adding BinaryTree stuff

--- Exercise_LinkedList/Exercise_LinkedList.cpp
+++ Exercise_LinkedList/Exercise_LinkedList.cpp
@@ -14,10 +14,12 @@ int main()
     list.pushFront(4);
     list.pushBack(5);
     list.pushBack(6);
+    list.pushBack(4);
+    list.pushBack(5);
     
-    list.insert(list.begin()->next->next->next, 5);
+    //list.insert(list.begin()->next->next->next, 5);
 
-    //list.clear();
+    list.remove(5);
     
 
     std::cout << list.count() << std::endl;
---
+++ Graphs/Graphs.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+
+
+class Graphs
+{
+public:
+    struct Node
+    {
+
+    };
+
+    struct Edge
+    {
+
+    };
+
+private:
+    std::list<Node*> m_nodes;
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+}
+
